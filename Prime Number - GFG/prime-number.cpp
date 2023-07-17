1859cc48889e3b04dc23bbe31e97dbe744e67306
Prashant Kumar <56153593+Prashant8722@users.noreply.github.com>
2023-07-18 00:57:08 +0530

    Added solution - LeetHub

--- Prime Number - GFG/prime-number.cpp
+++ Prime Number - GFG/prime-number.cpp
@@ -5,14 +5,29 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 public:
-    int isPrime(int n){
-       if(n == 1) return 0;
-       if(n == 2 || n == 3) return 1;
-       for(int i = 2 ; i <= sqrt(n) ; i++){
-           if(n % i == 0)   return false;
-       }
-       return true;
+    int isPrime(int N){
+      if (N <= 1) {
+        return false; // 0 and 1 are not prime numbers
     }
+
+    if (N <= 3) {
+        return true; // 2 and 3 are prime numbers
+    }
+
+    if (N % 2 == 0 || N % 3 == 0) {
+        return false; // Numbers divisible by 2 or 3 (except 2 and 3 themselves) are not prime
+    }
+
+    // Check for factors from 5 to the square root of N (considering only odd numbers)
+    for (int i = 5; i * i <= N; i += 6) {
+        if (N % i == 0 || N % (i + 2) == 0) {
+            return false; // N is divisible by i or i+2, so it is not a prime number
+        }
+    }
+
+    return true; // N is only divisible by 1 and itself, so it is a prime number
+}
+
 };
 
 
