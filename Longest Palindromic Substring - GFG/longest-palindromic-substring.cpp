b7ec549c32e3b84e81518aef44faa8c4b4748ca9
Prashant Kumar <56153593+Prashant8722@users.noreply.github.com>
2023-07-24 20:03:35 +0530

    Added solution - LeetHub

--- Longest Palindromic Substring - GFG/longest-palindromic-substring.cpp
+++ Longest Palindromic Substring - GFG/longest-palindromic-substring.cpp
@@ -6,7 +6,24 @@ using namespace std;
 
 // } Driver Code Ends
 //User function template for C++
+/*
+Algorithm:
 
+Initialize a 2D array dp of size n x n, where n is the length of the input string s. This array will be used to store whether a substring from index i to index j is a palindrome or not.
+
+Initialize two variables start and end to keep track of the starting and ending indices of the longest palindrome substring found so far. Initialize both start and end to 0 initially.
+
+Iterate over the indices of the input string s with two nested loops. The outer loop (i) represents the fixed ending position of the substring, and the inner loop (j) represents the starting position of the substring. We traverse the string in the upper triangular matrix of the dp array to avoid recomputation of already solved subproblems.
+
+Within the nested loops, check for three conditions to determine if the substring from index i to index j is a palindrome:
+
+If i == j, it means it is a single character, and thus, it is a palindrome (set dp[i][j] = 1).
+If i - j == 1, it means it is a substring of length 2. Check if the characters at indices i and j are the same. If they are, it is a palindrome (set dp[i][j] = 1).
+If s[i] == s[j] (the characters at the border of the substring are the same), and dp[i-1][j+1] (the substring inside the border) is also a palindrome, then the entire substring from i to j is a palindrome (set dp[i][j] = 1).
+While calculating the palindromes, update start and end whenever a longer palindrome is found.
+
+Finally, return the substring of s from index start to index end (inclusive), which represents the longest palindrome substring.
+*/
 class Solution{   
 public:
     string longestPalindrome(string s){
@@ -16,9 +33,9 @@ public:
         int dp[n][n] =  {0};
         int start = 0 , end = 0; //it tells starting and ending index of longrst substring
         
-        //start position of s for upper tringular matrix dp
+        //i fixed end position of s for upper tringular matrix dp
         for(int i = 0 ; i < n ; i++){
-            //fixed end position in s
+            //j fixed start position in s
             for(int j = i ; j >= 0; j--){   
                 if(i == j) dp[i][j] =1; //Diagonal filled it represents only one length of substring always;
                 
@@ -26,7 +43,7 @@ public:
                     if(s[i] == s[j]) dp[i][j] = 1;
                 }
                 
-                else if(s[i] == s[j] and dp[i -1][j+1] == 1){   //border character and substring inside border indicating by i and j
+                else if(s[i] == s[j] and dp[i - 1][j+1] == 1){   //border character and substring inside border indicating by i and j
                     dp[i][j] = 1;
                 }
                 //tracking maximum length for substring with index in s
