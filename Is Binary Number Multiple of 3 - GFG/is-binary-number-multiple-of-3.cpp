50c304d370005154e0b985eb3dad32c7eb975a56
Prashant Kumar <56153593+Prashant8722@users.noreply.github.com>
2023-09-16 18:41:09 +0530

    Added solution - LeetHub

---
+++ Is Binary Number Multiple of 3 - GFG/is-binary-number-multiple-of-3.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	int isDivisible(string s){
+	    //complete the function here
+	    /*
+	    This function works by counting the number of set bits at odd and even 
+	    positions in the binary number. If the difference between these counts 
+	    is a multiple of 3, then the binary number itself is a multiple of 3.
+	    */
+	    int evenCount = 0;
+	    int oddCount = 0;
+	    
+	    int len = s.length();
+	    for(int i = len - 1 ; i >= 0 ; i--){
+	        if(s[i] == '1'){
+	            if(i % 2 == 0 )
+	                evenCount += 1;
+                else{
+                    oddCount += 1;
+                }
+	        }
+	    }
+	    int res = abs(evenCount - oddCount);
+	    return res % 3 == 0 ?  1 : 0;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.isDivisible(s) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
